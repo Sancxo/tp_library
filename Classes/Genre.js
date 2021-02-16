a361d81e3acd6aaea2414e92a524a0a05840716d
Simon Tirant <simon.tirant@gmail.com>
2021-02-16 16:05:04 +0100

    fichiers de classe renommés

---
+++ Classes/Genre.js
@@ -0,0 +1,32 @@
+class Genre {
+    constructor(libelle, description) {
+        setLibelle(libelle);
+        setDescription(description);
+    }
+
+    setLibelle(libelle) {
+        if(libelle != null) {
+            libelle = libelle.trim();
+            if (libelle != "") {
+                if(libelle.length < 45) {
+                    this.libelle = libelle;
+                } else {
+                    throw new Error("Le champ 'Libelle' ne doit pas dépasser les 45 caractères de long.");
+                }
+            } else {
+                throw new Error("Le champ 'Libelle' ne peut pas être vide.");
+            }
+        } else {
+            throw new Error("Le champs 'Libelle' ne peut pas être null.");
+        }
+    }
+    getLibelle() { return this.libelle }
+
+    setDescription(description) {
+        this.description = description;
+    }
+
+    getDescription() { return this.description }
+}
+
+module.exports = Genre;
