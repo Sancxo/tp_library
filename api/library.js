4543c1eaec6a13a58da9fbe9be17c187c770eeb6
Simon Tirant <simon.tirant@gmail.com>
2021-02-16 16:13:31 +0100

    import des classes

--- api/library.js
+++ api/library.js
@@ -5,15 +5,8 @@ const router = express.Router();
 const knex = require("../db/knex");
 
 const Livre = require("../Classes/Livre");
-<<<<<<< HEAD
-
-// Test
-let livre = new Livre("test", "test", "test");
-console.log(livre);
-=======
 const Auteur = require('../Classes/Auteur');
 const Genre = require("../Classes/Genre");
->>>>>>> c78c42a (classes importées dans library.js)
 
 router.get("/livres", (req, res) => {
   knex
