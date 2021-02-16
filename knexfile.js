12aa3edbc513d0ea3ab3c7d676e545af03747b52
Simon Tirant <simon.tirant@gmail.com>
2021-02-16 16:06:45 +0100

    clean up commit

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
     version: "5.7",
     connection: {
       host: "localhost",
-      port: "8889",
+      port: "3306",
       user: "root",
       password: "root",
       database: "tp_php",
