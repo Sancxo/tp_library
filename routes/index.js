3588fb0ecd3aa2c8085bcc84640f1cc9f1e0c37e
Rem0ld <p.lovergne@hotmail.fr>
2021-02-17 11:29:42 +0100

    add connection, ejs engine and other goodies

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/index", function (req, res, next) {
+  if (req.session.loggedInUser) {
+    res.render("index", { username: req.session.username });
+  } else {
+    res.redirect("/login");
+  }
 });
 
 module.exports = router;
