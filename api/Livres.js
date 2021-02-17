60c54e0fe7b2edf369406cfbb55e949770ed5960
Simon Tirant <simon.tirant@gmail.com>
2021-02-17 16:06:20 +0100

    Transaction added on post

--- api/Livres.js
+++ api/Livres.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const knex = require("../db/knex");
 
 const Livre = require("../Classes/Livre");
+const { response } = require("express");
 
 //CRUD Livres
 router.get("/", (req, res) => {
@@ -14,37 +15,58 @@ router.get("/", (req, res) => {
   .join("possede", "livres.id_livres", "=", "possede.livres_id_livres")
   .join("genre", "id_genre", "=", "possede.genre_id_genre")
   .select("livres.*", "auteur.prenom", "auteur.nom", "genre.libelle", "genre.genre_description")
+  .select("livres.*", "auteur.prenom", "auteur.nom")
   .then((livres) => {
     res.json(livres);
   })
   .catch((err) => {
-    alert(err);
+    console.error(err);
   });
 });
 router.get("/:id", (req, res) => {
   knex("livres")
-    .select()
-    .where({ id_livres: req.params.id })
-    .then((livre) => {
-      res.json(livre);
-    })
-    .catch((err) => {
-      alert(err);
-    });
+  .select("livres.*", "auteur.prenom", "auteur.nom", "genre.libelle", "genre.genre_description")
+  .where({ id_livres: req.params.id })
+  .join("ecrit", "livres.id_livres", "=", "ecrit.livres_id_livres")
+  .join("auteur", "id_auteur", "=", "ecrit.auteur_id_auteur")
+  .join("possede", "livres.id_livres", "=", "possede.livres_id_livres")
+  .join("genre", "id_genre", "=", "possede.genre_id_genre")
+  .then((livre) => {
+    res.json(livre);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 });
 router.post("/", (req, res) => {
-  knex("livres")
+  knex.transaction((trx) => {
+    knex("livres")
+    .transacting(trx)
     .insert({
       titre: req.body.titre,
       livres_description: req.body.livre_desc,
-      image: req.body.image,
+      image: req.body.image
     })
-    .then(() => {
-      res.send("Nouveau livre créé : " + req.body.titre + ".");
+    .then((response) => {
+      let livreId = response[0];
+      return knex("ecrit")
+      .transacting(trx)
+      .insert({livres_id_livres: livreId, auteur_id_auteur: req.body.id_auteur})
+      .then(() => {
+        return knex("possede")
+        .transacting(trx)
+        .insert({livres_id_livres: livreId, genre_id_genre: req.body.id_genre});  
+      })
     })
-    .catch((err) => {
-      alert(err);
-    });
+    .then(trx.commit)
+    .catch(trx.rollback);
+  })
+  .then(() => {
+    res.send("Nouveau livre créé : " + req.body.titre + ".");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 });
 router.put("/:id", (req, res) => {
   knex("livres")
