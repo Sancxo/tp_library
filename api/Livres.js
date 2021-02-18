6fb752779ff9db350ce5342d2046b9d7ce16eda8
Simon Tirant <simon.tirant@gmail.com>
2021-02-18 09:15:13 +0100

    correction$

--- api/Livres.js
+++ api/Livres.js
@@ -6,13 +6,6 @@ const knex = require("../db/knex");
 
 const Livre = require("../Classes/Livre");
 
-const image = req.body.image ?? "";
-const title = req.body.title;
-const des = req.body.description;
-const idParam = req.params.id;
-const idGenre = req.body.id_genre;
-const idAuteur = req.body.id_auteur;
-
 //CRUD Livres
 router.get("/", (req, res) => {
   knex("livres")
@@ -21,7 +14,6 @@ router.get("/", (req, res) => {
   .join("possede", "livres.id_livres", "=", "possede.livres_id_livres")
   .join("genre", "id_genre", "=", "possede.genre_id_genre")
   .select("livres.*", "auteur.prenom", "auteur.nom", "genre.libelle", "genre.genre_description")
-  .select("livres.*", "auteur.prenom", "auteur.nom")
   .then((livres) => {
     res.json(livres);
   })
@@ -32,7 +24,7 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   knex("livres")
   .select("livres.*", "auteur.prenom", "auteur.nom", "genre.libelle", "genre.genre_description")
-  .where({ id_livres: idParam })
+  .where({ id_livres: req.params.id })
   .join("ecrit", "livres.id_livres", "=", "ecrit.livres_id_livres")
   .join("auteur", "id_auteur", "=", "ecrit.auteur_id_auteur")
   .join("possede", "livres.id_livres", "=", "possede.livres_id_livres")
@@ -45,6 +37,10 @@ router.get("/:id", (req, res) => {
   });
 });
 router.post("/", (req, res) => {
+  const image = req.body.image;
+  const title = req.body.title;
+  const des = req.body.description;
+
   knex.transaction((trx) => {
     knex("livres")
     .transacting(trx)
@@ -57,11 +53,11 @@ router.post("/", (req, res) => {
       let idLivre = response[0];
       return knex("ecrit")
       .transacting(trx)
-      .insert({livres_id_livres: idLivre, auteur_id_auteur: idAuteur})
+      .insert({livres_id_livres: idLivre, auteur_id_auteur: req.body.id_auteur})
       .then(() => {
         return knex("possede")
         .transacting(trx)
-        .insert({livres_id_livres: idLivre, genre_id_genre: idGenre});  
+        .insert({livres_id_livres: idLivre, genre_id_genre: req.body.id_genre});  
       })
     })
     .then(trx.commit)
@@ -76,6 +72,10 @@ router.post("/", (req, res) => {
   });
 });
 router.put("/:id", (req, res) => {
+  const image = req.body.image;
+  const title = req.body.title;
+  const des = req.body.description;
+  const idParam = req.params.id;
   knex.transaction((trx) => {
     knex("livres")
     .transacting(trx)
@@ -88,12 +88,12 @@ router.put("/:id", (req, res) => {
     .then(() => {
       return knex("ecrit")
       .transacting(trx)
-      .update({auteur_id_auteur: idAuteur})
+      .update({auteur_id_auteur: req.body.id_auteur})
       .where({livres_id_livres: idParam})
       .then(() => {
         return knex("possede")
         .transacting(trx)
-        .update({genre_id_genre: idGenre})
+        .update({genre_id_genre: req.body.id_genre})
         .where({livres_id_livres: idParam})
       })
     })
@@ -109,16 +109,34 @@ router.put("/:id", (req, res) => {
   });
 });
 router.delete("/:id", (req, res) => {
-  knex("livres")
+  const idParam = req.params.id;
+  knex.transaction((trx) => {
+    knex("livres")
+    .transacting(trx)
     .delete()
     .where({ id_livres: idParam })
     .then(() => {
-      res.render("add-form", { success: `Le livre n°${idParam} a bien été supprimé !` });
+      return knex("ecrit")
+      .transacting(trx)
+      .delete({auteur_id_auteur: req.body.id_auteur})
+      .where({livres_id_livres: idParam})
+      .then(() => {
+        return knex("possede")
+        .transacting(trx)
+        .delete({genre_id_genre: req.body.id_genre})
+        .where({livres_id_livres: idParam})
+      })
     })
-    .catch((err) => {
-      res.render("add-form", { error: `Le livre n°${idParam} n'a pu être supprimé.` });
-      console.error(err);
-    });
+    .then(trx.commit)
+    .catch(trx.rollback);
+  })
+  .then(() => {
+    res.render("add-form", { success: `Le livre n°${idParam} a bien été supprimé !` });
+  })
+  .catch((err) => {
+    res.render("add-form", { error: `Le livre n°${idParam} n'a pu être supprimé.` });
+    console.error(err);
+  });
 });
 
 module.exports = router;
